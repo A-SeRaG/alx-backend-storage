cf916988ed29713721db9ae45117c0185e7c8da5
SeRaG <aserag0101@gmail.com>
2024-10-18 02:58:56 +0300

    last

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,14 +1,14 @@
--- show users and update (or not) email
-SELECT * FROM users;
+-- Drop the trigger if it already exists
+DROP TRIGGER IF EXISTS email_change_reset;
 
-UPDATE users SET valid_email = 1 WHERE email = "bob@dylan.com";
-UPDATE users SET email = "sylvie+new@dylan.com" WHERE email = "sylvie@dylan.com";
-UPDATE users SET name = "Jannis" WHERE email = "jeanne@dylan.com";
-
-SELECT "--";
-SELECT * FROM users;
-
-UPDATE users SET email = "bob@dylan.com" WHERE email = "bob@dylan.com";
-
-SELECT "--";
-SELECT * FROM users;
+-- Create the trigger
+CREATE TRIGGER email_change_reset
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    -- Check if the email has changed
+    IF NEW.email <> OLD.email THEN
+        -- Reset valid_email to 0
+        SET NEW.valid_email = 0;
+    END IF;
+END;
